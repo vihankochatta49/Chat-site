8b285038b67eaa2e56d90c9b030e39358d42c267
Vihan <vihankochatta49@gmail.com>
2022-01-28 14:59:32 +0530

    Hosting

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,21 +1,33 @@
- const io = require('socket.io')(5006, { cors: { origin: '*', } });
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+// const io = require('socket.io')(5006, { cors: { origin: '*', } });
+const io = require("socket.io")(http);
 
-const users = {} // define for getting name of diff users with socket id
+const users = {}; // define for getting name of diff users with socket id
+var port = process.env.PORT || 3000;
 
-io.on('connection', socket => { 
-    socket.on('new-user-joined', name => { 
-        users[socket.id] = name; 
-        socket.broadcast.emit('user-joined', name) 
-    })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-    socket.on('send', message => {
-        socket.broadcast.emit('recieve', { message: message, name: users[socket.id] })
-    })
+  socket.on("send", (message) => {
+    socket.broadcast.emit("recieve", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
 
-    socket.on('disconnect', message => {
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id]
-    })
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
 
-})
+app.listen(port);
